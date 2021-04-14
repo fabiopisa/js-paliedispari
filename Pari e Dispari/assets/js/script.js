cf5075485a1599433e685dfcfbc4e552ad74ea3f
fabiopisa <pisanellifabio@gmail.com>
2021-04-14 16:02:27 +0200

    commit vinto o perso da finire

--- Pari e Dispari/assets/js/script.js
+++ Pari e Dispari/assets/js/script.js
@@ -8,24 +8,39 @@ Dichiariamo chi ha vinto. */
 var pariDispari = prompt("scegli pari o dispari");
 var numero_utente = parseInt(prompt("scegli un numero da 1 a 5"));
 var numero_cpu = random(1,5);
+var risultato = somma(numero_utente, numero_cpu)
 
-console.log(pariDispari);
-console.log(numero_utente);
-console.log(numero_cpu);
-console.log(somma(numero_utente, numero_cpu));
+console.log("La tua scelta è " + pariDispari);
+console.log("Il tuo numero è " + numero_utente);
+console.log("Il numero del cpu è " + numero_cpu);
+console.log("La somma fa " + risultato);
+console.log(vittoria(pariDispari, risultato))
 
 
 
 function random (min , max){
  var numero_rdm = Math.floor(Math.random() * ((max + 1) - min)) + min;
- console.log(numero_rdm)
+ return numero_rdm
 };
 
 function somma (n1, n2){
   var sum = n1 + n2;
   if(sum % 2 === 0){
-    return sum + " questa somma di numeri è pari"
+    return sum + ", questa somma di numeri è pari"
   }else{
-    return sum + " questa somma di numeri è dispari"
+    return sum + ", questa somma di numeri è dispari"
   };
 };
+
+function vittoria (scelta,cifra){
+  if(cifra % 2 === 0 && scelta == "pari"){
+    return "HAI VINTO"
+  }else if(cifra % 2 !==0 && scelta == "pari"){
+    return "HAI PERSO"
+  }else if(cifra % 2 ===0 && scelta == "dispari"){
+    return "HAI PERSO"
+  }else if(cifra % 2 !== 0 && scelta == "dispari"){
+    return "HAI VINTO"
+  }
+}
+
