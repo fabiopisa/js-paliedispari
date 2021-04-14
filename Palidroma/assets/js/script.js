2a60dd9f85a5f777e8ea527d673956cce708d46c
fabiopisa <pisanellifabio@gmail.com>
2021-04-14 14:16:49 +0200

    commit esercizio palindroma fatto

---
+++ Palidroma/assets/js/script.js
@@ -0,0 +1,16 @@
+/* **Palidroma**
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma */
+
+var parola = prompt("inserisci una parola");
+console.log(palindroma(parola));
+
+function palindroma (stringa){
+
+  if (stringa === stringa.split('').reverse().join('')){
+    return stringa + " è un parola palindroma";
+  }else {
+  return stringa + " non è una parola palindroma";
+  }
+  
+}
